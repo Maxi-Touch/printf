597ac43bd42839b0c9a60e9276d5ab660f5a1eff
Charlestimi <charlestimiebifemere@gmail.com>
2023-10-14 10:01:42 -0700

    print

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ *_printf - write to stdout formatted text
+ * @format: format specifer
+ * Return: no of bytes printed
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _printf(const char *format, ...);
+{
+         
+	va_list print_list
+	va_start(print_list,format)
+	va_arg(
+    
---
+++ main.h
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<ubistd.h>
+#include<stdarg.h>
+#include<string.h>
+
+int _printf(const char *format, ...);
+
+
+#endif
